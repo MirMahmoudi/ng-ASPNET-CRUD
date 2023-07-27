9bb260d774e4051ab64e6eade29cbe6f152f00c1
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-27 16:20:35 +0200

    DataBase & Migration

---
+++ API/Cards/Cards.API/Data/CardDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cards.API.Models;
+
+namespace Cards.API.Data
+{
+	public class CardDbContext : DbContext
+	{
+		// DbSets
+		public DbSet<Card> Cards { get; set; }
+
+		public CardDbContext(DbContextOptions options) :base(options) {}
+	}
+}
--- API/Cards/Cards.API/Program.cs
+++ API/Cards/Cards.API/Program.cs
@@ -1,3 +1,6 @@
+using Cards.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inject DbContext
+builder.Services.AddDbContext<CardDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("CardsConnection")));
 
 var app = builder.Build();
 
